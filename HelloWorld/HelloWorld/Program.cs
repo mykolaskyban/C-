d80668c8b7830369c29681a25ed4a6e3f1776adc
mykolaskyban <mykola.skyban@gmail.com>
2017-11-20 14:59:21 +0200

    my first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Niki
+ * Date: 20.11.2017
+ * Time: 12:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
